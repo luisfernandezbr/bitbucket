86d696d5825de3f1083a8e52aaa0ad29ad65c63e
Pedro Enrique <penrique@pinpoint.com>
2020-07-23 12:38:33 +0200

    small refactor

--- app/src/integration.tsx
+++ app/src/integration.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Icon, Loader, Error } from '@pinpt/uic.next';
+import { Icon, Loader, } from '@pinpt/uic.next';
 import {
 	useIntegration,
 	Account,
@@ -13,7 +13,6 @@ import {
 	FormType,
 	Http,
 	IOAuth2Auth,
-	Config,
 } from '@pinpt/agent.websdk';
 
 import styles from './styles.module.less';
@@ -26,71 +25,6 @@ interface workspacesResponse {
 	uuid: string;
 }
 
-async function workspacesToAccount(workspaces: workspacesResponse[], config: Config, auth: IAppBasicAuth | IOAuth2Auth): Promise<Account[]> {
-	let accounts: Account[] = [];
-	config.accounts = {}
-	for (var i = 0; i < workspaces.length; i++) {
-		var workspace = workspaces[i];
-		let count = 0;
-		try {
-			let res = await fetchRepoCount(workspace.slug, auth);
-			if (res[0] == 200) {
-				count = res[1];
-			} else {
-				console.error("error fetching repo count, response code", res[0])
-			}
-		} catch (ex) {
-			console.error("error fetching repo count", ex)
-		}
-		var obj: Account = {
-			avatarUrl: '',
-			totalCount: count,
-			id: workspace.uuid,
-			name: workspace.name,
-			description: workspace.slug,
-			type: 'ORG',
-			public: !workspace.is_private
-		};
-		accounts.push(obj);
-		config.accounts[obj.id] = obj;
-	}
-	return accounts;
-}
-
-const AccountList = ({ workspaces }: { workspaces: workspacesResponse[] | undefined }) => {
-	const { config, setConfig } = useIntegration();
-	const [accounts, setAccounts] = useState<Account[]>([]);
-	const [fetching, setFetching] = useState(false);
-
-	if (accounts.length === 0 && !fetching) {
-		const fetch = async () => {
-			let auth: IAppBasicAuth | IOAuth2Auth;
-			if (config.basic_auth) {
-				auth = config.basic_auth as IAppBasicAuth;
-			} else {
-				auth = config.oauth2_auth as IOAuth2Auth;
-			}
-			let res = await fetchWorkspaces(auth);
-			let accounts = await workspacesToAccount(res[1], config, auth);
-			setAccounts(accounts)
-
-			setConfig(config);
-			setFetching(false);
-		}
-		setFetching(true);
-		fetch();
-	}
-
-	return (
-		<AccountsTable
-			description='For the selected accounts, all repositories, pull requests and other data will automatically be made available in Pinpoint once installed.'
-			accounts={accounts}
-			entity='repo'
-			config={config}
-		/>
-	);
-};
-
 function createAuthHeader(auth: IAppBasicAuth | IOAuth2Auth): string {
 	var header: string;
 	if ('username' in auth) {
@@ -100,9 +34,8 @@ function createAuthHeader(auth: IAppBasicAuth | IOAuth2Auth): string {
 	var oauth = (auth as IOAuth2Auth);
 	return 'Bearer ' + oauth.access_token;
 }
-
 async function fetchWorkspaces(auth: IAppBasicAuth | IOAuth2Auth): Promise<[number, workspacesResponse[]]> {
-	var url = auth.url + '/2.0/workspaces'
+	var url = auth.url + '/2.0/workspaces';
 	var res = await Http.get(url, { 'Authorization': createAuthHeader(auth) });
 	if (res[1] === 200) {
 		return [res[1], res[0].values];
@@ -118,6 +51,81 @@ async function fetchRepoCount(reponame: string, auth: IAppBasicAuth | IOAuth2Aut
 	return [res[1], 0]
 }
 
+const AccountList = ({ workspaces, setWorkspaces }: { workspaces: workspacesResponse[], setWorkspaces: (val: workspacesResponse[]) => void }) => {
+	const { config, setConfig } = useIntegration();
+	const [accounts, setAccounts] = useState<Account[]>([]);
+	const [fetching, setFetching] = useState(false);
+
+	let auth: IAppBasicAuth | IOAuth2Auth;
+	if (config.basic_auth) {
+		auth = config.basic_auth as IAppBasicAuth;
+	} else {
+		auth = config.oauth2_auth as IOAuth2Auth;
+	}
+
+	useEffect(() => {
+		if (fetching || accounts.length || !workspaces.length) {
+			return
+		}
+		setFetching(true);
+		const fetch = async () => {
+			config.accounts = {}
+			for (var i = 0; i < workspaces.length; i++) {
+				var workspace = workspaces[i];
+				let count = 0;
+				try {
+					let res = await fetchRepoCount(workspace.slug, auth);
+					if (res[0] == 200) {
+						count = res[1];
+					} else {
+						console.error('error fetching repo count, response code', res[0]);
+					}
+				} catch (ex) {
+					console.error('error fetching repo count', ex);
+				}
+				var obj: Account = {
+					avatarUrl: '',
+					totalCount: count,
+					id: workspace.uuid,
+					name: workspace.name,
+					description: workspace.slug,
+					type: 'ORG',
+					public: !workspace.is_private
+				};
+				accounts.push(obj);
+				config.accounts[obj.id] = obj;
+			}
+			setConfig(config);
+			setAccounts(accounts)
+			setFetching(false);
+		}
+		fetch();
+	}, [workspaces]);
+
+	useEffect(() => {
+		if (workspaces.length) {
+			return;
+		}
+		const fetch = async () => {
+			let res = await fetchWorkspaces(auth);
+			if (res[0] === 200) {
+				setWorkspaces(res[1]);
+			} else {
+				console.error('error fetching projects. responde code', res[0]);
+			}
+		}
+		fetch();
+	}, [config.apikey_auth, config.oauth2_auth]);
+
+	return (
+		<AccountsTable
+			description='For the selected accounts, all repositories, pull requests and other data will automatically be made available in Pinpoint once installed.'
+			accounts={accounts}
+			entity='repo'
+			config={config}
+		/>
+	);
+};
 
 const LocationSelector = ({ setType }: { setType: (val: IntegrationType) => void }) => {
 	return (
@@ -138,27 +146,26 @@ const LocationSelector = ({ setType }: { setType: (val: IntegrationType) => void
 const SelfManagedForm = ({ setWorkspaces }: { setWorkspaces: (val: workspacesResponse[]) => void }) => {
 	async function verify(auth: IAuth): Promise<boolean> {
 		try {
-			var res = await fetchWorkspaces(auth as IAppBasicAuth)
+			var res = await fetchWorkspaces(auth as IAppBasicAuth);
 			if (res[0] === 200) {
 				setWorkspaces(res[1]);
-				return true
+				return true;
 			}
-			console.error("error fetching workspaces, response code", res[0])
-			return false
+			console.error('error fetching workspaces, response code', res[0]);
+			return false;
 		} catch (ex) {
-			console.error("error fetching workspaces", ex)
-			return false
+			console.error('error fetching workspaces', ex);
+			return false;
 		}
 	}
 	return <Form type={FormType.BASIC} name='bitbucket' callback={verify} />;
 };
 
 const Integration = () => {
-	const { loading, currentURL, config, isFromRedirect, isFromReAuth, setConfig, authorization } = useIntegration();
+	const { loading, currentURL, config, isFromRedirect, isFromReAuth, setConfig } = useIntegration();
 	const [type, setType] = useState<IntegrationType | undefined>(config.integration_type);
 	const [, setRerender] = useState(0);
-	const [workspaces, setWorkspaces] = useState<workspacesResponse[]>()
-
+	const [workspaces, setWorkspaces] = useState<workspacesResponse[]>([]);
 
 	// ============= OAuth 2.0 =============
 	useEffect(() => {
@@ -183,14 +190,15 @@ const Integration = () => {
 			});
 		}
 
+	}, [loading, isFromRedirect, currentURL]);
 
+	useEffect(() => {
 		if (type) {
 			config.integration_type = type;
 			setConfig(config);
 			setRerender(Date.now());
 		}
-
-	}, [loading, isFromRedirect, currentURL, config, setRerender, setConfig, type]);
+	}, [type])
 
 	if (loading) {
 		return <Loader screen />;
@@ -200,7 +208,7 @@ const Integration = () => {
 
 	if (isFromReAuth) {
 		if (config.integration_type === IntegrationType.CLOUD) {
-			content = <OAuthConnect name='BitBucket' reauth />
+			content = <OAuthConnect name='BitBucket' reauth />;
 		} else {
 			content = <SelfManagedForm setWorkspaces={setWorkspaces} />;
 		}
@@ -212,7 +220,7 @@ const Integration = () => {
 		} else if (config.integration_type === IntegrationType.SELFMANAGED && !config.basic_auth && !config.apikey_auth) {
 			content = <SelfManagedForm setWorkspaces={setWorkspaces} />;
 		} else {
-			content = <AccountList workspaces={workspaces} />
+			content = <AccountList workspaces={workspaces} setWorkspaces={setWorkspaces} />;
 		}
 	}
 
@@ -220,7 +228,7 @@ const Integration = () => {
 		<div className={styles.Wrapper}>
 			{content}
 		</div>
-	)
+	);
 };
 
 
